a23d21406c022659aac0147bce61cbf258e775e8
jaderyan <jadeelizabethryan@outlook.com>
2020-06-10 16:27:05 +0100

    add description for collection

--- src/views/Results/ListView/List.tsx
+++ src/views/Results/ListView/List.tsx
@@ -5,39 +5,49 @@ import SideboxCard from '../SideboxCard';
 import find from 'lodash/find';
 import SearchResultCard from '../../../components/SearchResultCard';
 import ResultsStore from '../../../stores/resultsStore';
+import get from 'lodash/get';
 
 interface IProps {
   title: string;
+
   hasSideboxes: (titile: string) => ISidebox[] | null;
   resultsList: IService[];
   resultsStore: ResultsStore;
 }
 
-const List: FunctionComponent<IProps> = ({ title, hasSideboxes, resultsList, resultsStore }) => (
-  <Fragment key={title}>
-    <div className="results__container">
-      {resultsStore.isCategorySearch && (
-        <div className="flex-col--12">
-          <h1 className="results__container__title">{title.replace('Homepage:', '')}</h1>
-        </div>
-      )}
-      <div className="flex-col--12">
-        {hasSideboxes(title) && (
-          <div className="flex-container flex-container--mobile-no-padding results__category-sidebar">
-            {map(hasSideboxes(title), (sidebox: ISidebox) => {
-              return <SideboxCard sidebox={sidebox} />;
-            })}
+const List: FunctionComponent<IProps> = ({ title, hasSideboxes, resultsList, resultsStore }) => {
+  const category = find(resultsStore.categories, ['name', title]);
+  const categoryDescription = get(category, 'intro');
+
+  return (
+    <Fragment key={title}>
+      <div className="results__container">
+        {resultsStore.isCategorySearch && (
+          <div className="flex-col--12">
+            <h1 className="results__container__title">{title.replace('Homepage:', '')}</h1>
+            {title !== categoryDescription && categoryDescription && (
+              <p className="results__container__description">{categoryDescription}</p>
+            )}
           </div>
         )}
+        {hasSideboxes(title) && (
+          <div className="flex-col--12">
+            <div className="flex-container flex-container--mobile-no-padding results__category-sidebar">
+              {map(hasSideboxes(title), (sidebox: ISidebox) => {
+                return <SideboxCard sidebox={sidebox} />;
+              })}
+            </div>
+          </div>
+        )}
+        {resultsList.map((list: any) => {
+          const organisation =
+            find(resultsStore.organisations, ['id', list.organisation_id]) || null;
+
+          return <SearchResultCard key={list.id} result={list} organisation={organisation} />;
+        })}
       </div>
-      {resultsList.map((list: any) => {
-        const organisation = find(resultsStore.organisations, ['id', list.organisation_id]) || null;
-
-        return <SearchResultCard key={list.id} result={list} organisation={organisation} />;
-      })}
-    </div>
-    {resultsStore.isCategorySearch && <hr className="results__divider" />}
-  </Fragment>
-);
-
+      {resultsStore.isCategorySearch && <hr className="results__divider" />}
+    </Fragment>
+  );
+};
 export default List;
