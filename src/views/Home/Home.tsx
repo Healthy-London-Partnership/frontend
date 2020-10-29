b70f3973c393ac4d32c3fcb6fca738fdeafcd869
Sean Jessen <info@seanjessen.co.uk>
2020-10-29 22:46:05 +0000

    Updated the about copy on the homepage

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -14,8 +14,7 @@ const Home: React.FunctionComponent = () => {
           <div className="flex-col flex-col--10">
             <div className="home-about-wrapper">
               <h2 className="home-about-title">About Connect</h2>
-              <p>NHS Connect is a new service, pioneered by the NHS and leading Councils, that gives you the ability to find the health and wellbeing support that meets your needs.</p>
-              <p>Connect is currently available in a small group of pilot London boroughs: Camden, Merton, Hillingdon, Kensington and Chelsea, Southwark and Westminster. We’ll be adding many more local areas at the end of 2020; prior to that we’ll continue to make lots of improvements to the system as a whole.</p>
+              <p>We're here to connect you with the health, care and wellbeing support that best meets your needs. We're in the process of adding new support opportunities, so if you can't find what you are looking for this time, please come back soon for another look.</p>
               <Link
                 text="Read more"
                 href="/about-connect"
