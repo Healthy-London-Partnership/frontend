43eb14569b1d594c39c6f40b907b259c121a48cb
ppbiriukovdev <ppbiriukov.dev@gmail.com>
2021-02-15 09:23:01 +1000

    Remove blue outline in search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -38,9 +38,15 @@ class Search extends React.Component<IProps> {
           <form className="flex-col--tablet--12 flex-col--10 search__inner-container">
             <div className="flex-container flex-container--mobile-no-padding">
               <div className="flex-col--12">
-                <h1 className="search__heading">{get(cmsStore, 'home.search_title')}</h1>
+                {/*<h1 className="search__heading">{get(cmsStore, 'home.search_title')}</h1>*/}
+                <h1 className="search__heading">Find support and advice</h1>
               </div>
-              <SearchInput showButtonText={true} showGeoLocate={true} keywordFieldLabel="Enter a keyword" postcodeFieldLabel="Enter a location" />
+              <SearchInput
+                showButtonText={true}
+                showGeoLocate={true}
+                keywordFieldLabel="Enter a keyword"
+                postcodeFieldLabel="Enter a location"
+              />
             </div>
           </form>
         </section>
